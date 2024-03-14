6b4e0b4ad74219b289da9061c6c0b2d4b2a7d477
duducg <edu.sang.rocha@gmail.com>
2024-03-14 21:03:18 +0000

    Added XML to Main and Decor

--- BetterDecorator/Program.cs
+++ BetterDecorator/Program.cs
@@ -4,11 +4,13 @@ namespace BetterDecorator
 {
     class Program
     
-    {
-        static void Main(string[] args)
+    {/// <summary>
+    /// Takes in 3 arguments and prints an interpolated string to Console.
+    /// </summary>
+    /// <param name="args">string array passed in via CMD</param>
+        private static void Main(string[] args)
         {
-            //take in 3 arguments from args[]
-            //parsing
+
             string s_ = args[0];
             char dec_ = char.Parse(args[1]);
             int rep_ = int.Parse(args[2]);
@@ -17,7 +19,13 @@ namespace BetterDecorator
 
             
         }
-        //Decor() - takes in "int" that indicates how many "char"
+        /// <summary>
+        /// Takes in a string, a char and an int argument.
+        /// </summary>
+        /// <param name="s">string that will sit between the chars</param>
+        /// <param name="dec">chars that will repeat</param>
+        /// <param name="rep">Number of repetitions for the char</param>
+        /// <returns>An interpolated string</returns>
             private static string Decor(string s, char dec, int rep)
             {
                 //build the prefix and suffix
