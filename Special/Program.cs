644127413190c78e7ac4c11afb7d2b8674035899
duducg <edu.sang.rocha@gmail.com>
2024-03-15 21:23:53 +0000

    Finished GetSpecial Method

--- Special/Program.cs
+++ Special/Program.cs
@@ -6,7 +6,33 @@ namespace Special
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //invoke GetMethod() here.
+            Console.Write("Insert n-th number of the special sequence: ");
+            int user_in = int.Parse(Console.ReadLine());
+            Console.WriteLine(GetSpecial(user_in));
+        }
+        //recursive method
+        private static int a = 0, b = 1,c;
+        private static int GetSpecial(int n)
+        {       
+
+
+            //n is the number of iterations this goes for
+            if (n == 0 || n == 1)
+            {
+                return n;
+            }
+            else
+            {
+                c = b;
+                b = b  + (a * 2);
+                a = c;
+                GetSpecial(n - 1);//solely used as a counter
+            }
+            return b;
+        
+
+
         }
     }
 }
